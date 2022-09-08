78cad11b4f9136076e1a3b7f7068f8a0412e2861
Teklay Birhane <90598109+Tekhlay@users.noreply.github.com>
2022-09-08 11:03:01 +0300

    Create 9-full_creation.sql

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,5 @@
+-- Script should create 4 records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
